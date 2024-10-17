cac182d372fb7584566fef7390317e19e947e2bc
ManoGalas <galisavictor@gmail.com>
2024-10-17 11:31:02 -0300

    GameManager

--- Carrinho/Assets/Scripts/GameManager.cs
+++ Carrinho/Assets/Scripts/GameManager.cs
@@ -1,8 +1,11 @@
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class GameManager : MonoBehaviour
+
+public class GameManager : MonoBehaviourPun
 {
     #region Singleton
     public static GameManager instance;
@@ -20,4 +23,62 @@ public class GameManager : MonoBehaviour
 
     }
     #endregion
+
+  
+    public GameObject playerPrefab;
+    [SerializeField]Transform TransformPosition;
+   
+
+    private bool gameEnded = false;
+
+    private Dictionary<string, int> playerScores = new Dictionary<string, int>();
+
+    void Start()
+    {
+        instance = this;
+
+
+        // Cria um novo jogador na rede
+        if (PhotonNetwork.IsConnectedAndReady)
+        {
+            PhotonNetwork.Instantiate(playerPrefab.name,TransformPosition.position , Quaternion.identity);
+        }
+
+    }
+
+    // Atualiza a pontuação de cada jogador
+    public void UpdateScore(string playerName)
+    {
+        photonView.RPC("RPCUpdateScore", RpcTarget.All, playerName);
+    }
+
+    // Método chamado quando o jogo termina
+    void EndGame()
+    {
+        gameEnded = true;
+        string winner = GetWinner();
+        Debug.Log("Vencedor: " + winner);
+        // Aqui você pode adicionar lógica para mostrar o vencedor na UI
+    }
+
+    // Determina quem ganhou
+    string GetWinner()
+    {
+        string winner = "";
+        int highestScore = 0;
+
+        foreach (var playerScore in playerScores)
+        {
+            if (playerScore.Value > highestScore)
+            {
+                highestScore = playerScore.Value;
+                winner = playerScore.Key;
+            }
+        }
+
+        return winner;
+    }
+
+
 }
+

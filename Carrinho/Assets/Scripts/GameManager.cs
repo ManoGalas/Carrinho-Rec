7e4ca10ed90c025b06586a2823a26232ea1ca20d
ManoGalas <galisavictor@gmail.com>
2024-10-17 11:07:10 -0300

    Singleton
    Singleton

---
+++ Carrinho/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    #endregion
+}
